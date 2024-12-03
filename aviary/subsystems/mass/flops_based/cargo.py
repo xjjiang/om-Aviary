b69bae6c16e02dee45c86957c97770ee02a4f8b6
Xun Jiang <xun.j.jiang@nasa.gov>
2024-12-03 11:24:19 -0800

    minor update

--- aviary/subsystems/mass/flops_based/cargo.py
+++ aviary/subsystems/mass/flops_based/cargo.py
@@ -7,7 +7,7 @@ import openmdao.api as om
 
 from aviary.utils.aviary_values import AviaryValues
 from aviary.variable_info.functions import add_aviary_input, add_aviary_output
-from aviary.variable_info.variables import Aircraft, Mission
+from aviary.variable_info.variables import Aircraft
 
 
 class CargoMass(om.ExplicitComponent):
