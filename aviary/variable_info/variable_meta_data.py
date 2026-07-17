7ba80d6cf263c5eb0c42fe2199e25c8b854d2335
Xun Jiang <xun.j.jiang@nasa.gov>
2026-07-17 12:20:31 -0400

    minor update

--- aviary/variable_info/variable_meta_data.py
+++ aviary/variable_info/variable_meta_data.py
@@ -2342,7 +2342,6 @@ add_meta_data(
     desc='Unscaled mass of a single engine. See Aircraft.Engine.MASS for breakdown of what is '
     'included in engine mass.',
     default_value=0.0,
-    option=False,
     multivalue=True,
 )
 
